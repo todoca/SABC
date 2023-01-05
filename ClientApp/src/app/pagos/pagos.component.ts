8b934c08260e5d03c3e15377eebf0afe69d8fbf1
Todoca by Alejandro Marcano <info@todoca.com.ve>
2023-01-04 22:18:53 -0400

    Front end con Opcion de busqueda directo al procedimiento almacenado a traves del webservice

--- ClientApp/src/app/pagos/pagos.component.ts
+++ ClientApp/src/app/pagos/pagos.component.ts
@@ -1,30 +1,39 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-pagos',
   templateUrl: './pagos.component.html'
 })
 export class PagosComponent {
   public clientes: Cliente[] = [];
-  public pagosList: Pago[] = [];
+  public cedulaBuscar = '';
+  public ultimaCedula = '';
+  public mensaje = '';
   
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  constructor(private http: HttpClient, @Inject('BASE_URL')  private baseUrl: string) {
     http.get<Cliente[]>(baseUrl + 'pagos').subscribe(result => {
       this.clientes = result;
     }, error => console.error(error));
   }
+
+  public buscarcliente() {
+    this.ultimaCedula = this.cedulaBuscar;
+    this.http.get<Cliente[]>(this.baseUrl + 'pagos/'+this.cedulaBuscar).subscribe(result => {
+      this.clientes = result;
+      this.cedulaBuscar = '';
+      if (this.clientes.length == 0) this.mensaje = 'Cliente no encontrado... por favor vuelve a intentar.';
+      else this.mensaje = '';
+    }, error => console.error(error));
+  }
 }
 interface Cliente {
-  id?: number;
   nombreCompleto: string;
   cedula: string;
-  pin: string;
-  pagos?: Pago[];
+  fecha: string;
+  monto: number;
 }
-interface Pago {
-  fecha: string,
-  monto:number
-}
+
 
